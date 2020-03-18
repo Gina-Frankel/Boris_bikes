a4b94a5cfca5503cfc899608980c78f06d7c2b93
Edward Phillips <edd0ddi0@gmail.com>
2020-03-18 16:50:55 +0000

    modified Docking_station.rb so that it releases bike objects as defined in bike.rb & associated rspec tests

--- Boris_Bikes/spec/docking_station_spec.rb
+++ Boris_Bikes/spec/docking_station_spec.rb
@@ -3,6 +3,9 @@ require 'docking_station'
 describe DockingStation do
 
    it { is_expected.to respond_to :release_bike }
+    docking_station = DockingStation.new
+   daisy = docking_station.release_bike
+   it { expect(daisy).to respond_to :working?}
 
 
 end
