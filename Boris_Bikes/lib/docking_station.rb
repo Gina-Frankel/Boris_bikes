1b788df9f81218935dca2f64842cb1c0ae892aa4
Gina Frankel <g.frankel22@gmail.com>
2020-03-19 15:51:04 +0000

    modified docking_station.rb & spec/docking_station_spec.rb

--- Boris_Bikes/lib/docking_station.rb
+++ Boris_Bikes/lib/docking_station.rb
@@ -1,8 +1,29 @@
 require './lib/bike'
 class DockingStation
 
+  attr_reader :storage
+
+  def initialize
+    @storage = []
+  end
+
   def release_bike
-    Bike.new
+    # if the storage has no bikes then dont release bike
+    if @storage.empty?
+      fail "no bike available"
+    else
+      return Bike.new
+    end
+  end
+
+  def dock_bikes(arg)
+    #created @daisy
+    @storage.push(arg)
+  end
+
+  def look_at_bike
+    #@daisy stored
+    @storage
   end
 
 end
--- Boris_Bikes/spec/docking_station_spec.rb
+++ Boris_Bikes/spec/docking_station_spec.rb
@@ -1,11 +1,29 @@
 require 'docking_station'
 
 describe DockingStation do
+docking_station = DockingStation.new
+let(:daisy){Bike.new} # indepedent bike
 
-   it { is_expected.to respond_to :release_bike }
-    docking_station = DockingStation.new
-   daisy = docking_station.release_bike
-   it { expect(daisy).to respond_to :working?}
+  it { is_expected.to respond_to :release_bike }
+  #it - refering to describe DockingStation do
 
+  it {expect(docking_station).to respond_to(:dock_bikes).with(1)}
+
+  it do
+    #subject(:daisy) {docking_station.release_bike}
+    expect(daisy).to respond_to :working?
+  end
+
+  it "docking station releases bike with no bike" do
+    #subject(:daisy) { docking_station.release_bike }
+    expect { docking_station.release_bike}.to raise_error "no bike availble"#raise_error
+  end
+     # look at bike
+
+  it do
+  #subject(:daisy) { docking_station.dock_bikes}
+  docking_station.dock_bikes(daisy)
+  expect (docking_station.look_at_bike).to include(daisy)
+  end
 
 end
